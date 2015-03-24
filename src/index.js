8c0706fde355336111f779318cfb36ab692c15d3
Daniel <daniel.berndt@gmail.com>
2015-03-24 11:20:54 +0100

    move require statements to top

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+var loaderUtils = require("loader-utils");
+var gm = require("gm");
+var extend = require("extend");
+
 var DEFAULTOPTIONS = {
 	skip: false
 }
 module.exports = function(content) {
-	var loaderOptions = require('loader-utils').parseQuery(this.query);
-	loaderOptions = require('extend')(true,DEFAULTOPTIONS, loaderOptions);
+	var loaderOptions = loaderUtils.parseQuery(this.query);
+	loaderOptions = extend(true,DEFAULTOPTIONS, loaderOptions);
 	if ( loaderOptions.skip ) {
 		return content;
 	}
-	var options = require('loader-utils').parseQuery(this.resourceQuery), cb = this.async(), width = options['max-width'], height = options['max-height'];
-	require('gm')(content, this.resource).size(function(err, value){
+	var options = loaderUtils.parseQuery(this.resourceQuery), cb = this.async(), width = options['max-width'], height = options['max-height'];
+	gm(content, this.resource).size(function(err, value){
 		if ( err ) {
 			return cb(err);
 		}
